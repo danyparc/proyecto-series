3c6643b41ca8ce721e5e2e704970c864be6944c5
DDoS <oxcarhh@gmail.com>
2021-06-09 21:36:07 -0500

    mi perfil

--- src/colaboradores.js
+++ src/colaboradores.js
@@ -3,5 +3,10 @@ export const colaboradores = [
     name: 'Daniel Garcia',
     title: 'Dev Lead',
     bio: 'Sensei de la Generacion 6 Grupo B del Master en Code de dev.f'
-  }
+  },
+  {
+    name: 'Oscar Hernández',
+    title: 'Dev',
+    bio: 'Alumno de la G6 del grupo B'
+  },
 ]
