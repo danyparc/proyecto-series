36b9c354d72e9a10143c958cc34d6800b33f64c9
MiguelM <miguelmonterroso39@gmail.com>
2021-06-09 20:35:21 -0600

    Agregue a Miguel Monterroso

---
+++ src/colaboradores.js
@@ -0,0 +1,12 @@
+export const colaboradores = [
+  {
+    name: 'Daniel Garcia',
+    title: 'Dev Lead',
+    bio: 'Sensei de la Generacion 6 Grupo B del Master en Code de dev.f'
+  },
+  {
+    name: 'Miguel Monterroso',
+    title: 'Collaborator',
+    bio: 'Estudiante dev.f'
+  }
+]
