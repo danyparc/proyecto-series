194946f91cbb0bb3ef4eac910151a5b0d85573ef
danyparc <danyparc@gmail.com>
2021-06-09 21:35:19 -0500

    agregue a mi clon de sombras

--- src/colaboradores.js
+++ src/colaboradores.js
@@ -3,5 +3,10 @@ export const colaboradores = [
     name: 'Daniel Garcia',
     title: 'Dev Lead',
     bio: 'Sensei de la Generacion 6 Grupo B del Master en Code de dev.f'
+  },
+  {
+    name: 'Danyparc',
+    title: 'Dev',
+    bio: 'Clon de sombras de Daniel Garcia'
   }
 ]
