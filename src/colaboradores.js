2ebae30622012f1635c47e275dd1a0afe915c4c4
DianaLauraDev <streetwalkerpot@gmail.com>
2021-06-10 01:52:34 -0500

    Agregué mi perfíl.

--- src/colaboradores.js
+++ src/colaboradores.js
@@ -3,5 +3,11 @@ export const colaboradores = [
     name: 'Daniel Garcia',
     title: 'Dev Lead',
     bio: 'Sensei de la Generacion 6 Grupo B del Master en Code de dev.f'
+  },
+
+{
+name: 'DianaLaura',
+title: 'Dev',
+bio: 'Estudiante del Master en Coding G6-B'
   }
 ]
