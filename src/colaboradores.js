3c5e352800d591734fb8242aa7e41d928dd6e26e
EyYiCi <ing.alejandragc@gmail.com>
2021-06-09 21:43:05 -0500

    mi primer commit de agc

--- src/colaboradores.js
+++ src/colaboradores.js
@@ -3,5 +3,10 @@ export const colaboradores = [
     name: 'Daniel Garcia',
     title: 'Dev Lead',
     bio: 'Sensei de la Generacion 6 Grupo B del Master en Code de dev.f'
+  },
+  {
+    name: 'Alejandra Garcia',
+    title: 'En las calles me dicen EyYiCi',
+    bio: 'Alumna que no termina las practicas y ocupara las vacaciones para ponerse al dia'
   }
 ]
