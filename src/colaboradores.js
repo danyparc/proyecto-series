2674c6470b0b920a22e8108ecfba0a74fd1c2cd0
Gustavo <gustavo.8805@gmail.com>
2021-06-09 21:39:27 -0500

    agregue mi informacion

--- src/colaboradores.js
+++ src/colaboradores.js
@@ -3,5 +3,10 @@ export const colaboradores = [
     name: 'Daniel Garcia',
     title: 'Dev Lead',
     bio: 'Sensei de la Generacion 6 Grupo B del Master en Code de dev.f'
+  },
+  {
+    name: 'Gustavo Barrera',
+    title: 'Frontend',
+    bio: 'Frontend Developer'
   }
 ]
