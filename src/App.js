5295996af24bd5776479822a1cb05f2043285f0c
daniel-gm <daniel.gm@globant.com>
2021-06-09 21:11:04 -0500

    agregue lista de colaboradores

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
-
+import { colaboradores } from './colaboradores'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hola Mundo</h1>
+        <h1>Colaboradores:</h1>
+        {
+          colaboradores.map((colab)=>(
+            <div className="card">
+              <p className="name">{colab.name}</p>
+              <p className="title">{colab.title}</p>
+              <p className="bio">{colab.bio}</p>
+            </div>
+          ))
+        }
       </header>
     </div>
   );
---
+++ src/colaboradores.js
@@ -0,0 +1,7 @@
+export const colaboradores = [
+  {
+    name: 'Daniel Garcia',
+    title: 'Dev Lead',
+    bio: 'Sensei de la Generacion 6 Grupo B del Master en Code de dev.f'
+  }
+]
